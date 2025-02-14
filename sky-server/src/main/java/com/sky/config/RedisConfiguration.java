f77ed0b4e838bec45f0798a49b58c0c94f2a9558
LuciusWan <18099488938@163.com>
2025-02-14 14:11:45 +0800

    Redis的简单应用

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,31 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@Configuration
+public class RedisConfiguration {
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("开始创建redis模板对象");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        //设置redis连接工厂
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置redis key序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // 设置值的序列化器
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        // 设置哈希键和哈希值的序列化器
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+}
